775d84396f367681b88da551c5a130e70033040b
RSamaium <sam@webcreative5.net>
2023-02-02 15:33:08 +0100

    users component

--- src/app/users/users.component.spec.ts
+++ src/app/users/users.component.spec.ts
@@ -1,23 +1,34 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from "@angular/common/http"
+import { HttpClientTestingModule } from "@angular/common/http/testing"
+import { ComponentFixture, TestBed } from "@angular/core/testing"
+import { UsersComponent } from "./users.component"
 
-import { UsersComponent } from './users.component';
-
-describe('UsersComponent', () => {
-  let component: UsersComponent;
-  let fixture: ComponentFixture<UsersComponent>;
+describe('Tester les utilisateurs', () => {
+  let fixture: ComponentFixture<UsersComponent>
+  let component: UsersComponent
+  let tpl: HTMLElement
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UsersComponent ]
-    })
-    .compileComponents();
+      declarations: [UsersComponent],
+      imports: [HttpClientModule]
+    }).compileComponents()
 
-    fixture = TestBed.createComponent(UsersComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(UsersComponent)
+    fixture.detectChanges()  // ngOnInit
+    await fixture.whenStable()
+    fixture.detectChanges()
+    component = fixture.componentInstance
+    tpl = fixture.nativeElement
+  })
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  it('Vérifier que liste utilisateur bien affichée',  async() => {
+    const cardsEl = tpl.querySelectorAll('.card')
+    expect(component.users.length).toBeGreaterThan(0)
+    expect(cardsEl.length).toBe(component.users.length)
+  })
+
+  it('', () => {
+
+  })
+})
